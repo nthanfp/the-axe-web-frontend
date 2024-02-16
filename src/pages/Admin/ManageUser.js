667991832a4093f5020c631bed5d40742b5e9375
Nathanael Ferry Pratama <ntnferry1505@gmail.com>
2024-02-17 00:51:44 +0700

    Add stat card

--- src/pages/Admin/ManageUser.js
+++ src/pages/Admin/ManageUser.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faKey, faUser, faPlus, faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faKey, faUser, faPlus, faEdit, faTrash, faCommentAlt, faChartBar, faMousePointer, faPlusSquare } from '@fortawesome/free-solid-svg-icons';
 import moment from 'moment';
 import Swal from 'sweetalert2';
 import $ from 'jquery';
@@ -32,7 +32,7 @@ const ManageUser = () => {
 
   useEffect(() => {
     fetchUserList();
-    $('#userTable').DataTable();
+    // $('#userTable').DataTable();
   }, []);
 
   const getBadgeColor = (role) => {
@@ -102,6 +102,75 @@ const ManageUser = () => {
           <div className='col-md-12 mb-3'>
             <SelectBar />
           </div>
+          <div className='col-md-12 mb-3'>
+            <div className="row">
+        
+              <div className="col-xl-3 col-md-6 mb-2">
+                <div className="card border border-primary">
+                  <div className="card-body">
+                    <div className="d-flex justify-content-between align-items-center">
+                      <div className="text-left">
+                        <h3 className="mb-0">278</h3>
+                        <span>Total User</span>
+                      </div>
+                      <div className="float-end">
+                        <FontAwesomeIcon icon={faUser} className='bg-primary p-2 rounded-4 text-white' size="2x" />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-xl-3 col-md-6 mb-2">
+                <div className="card border border-primary">
+                  <div className="card-body">
+                    <div className="d-flex justify-content-between align-items-center">
+                      <div className="text-left">
+                        <h3 className="mb-0">278</h3>
+                        <span>Total User</span>
+                      </div>
+                      <div className="float-end">
+                        <FontAwesomeIcon icon={faUser} className='bg-primary p-2 rounded-4 text-white' size="2x" />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-xl-3 col-md-6 mb-2">
+                <div className="card border border-primary">
+                  <div className="card-body">
+                    <div className="d-flex justify-content-between align-items-center">
+                      <div className="text-left">
+                        <h3 className="mb-0">278</h3>
+                        <span>Total User</span>
+                      </div>
+                      <div className="float-end">
+                        <FontAwesomeIcon icon={faUser} className='bg-primary p-2 rounded-4 text-white' size="2x" />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-xl-3 col-md-6 mb-2">
+                <div className="card border border-primary">
+                  <div className="card-body">
+                    <div className="d-flex justify-content-between align-items-center">
+                      <div className="text-left">
+                        <h3 className="mb-0">278</h3>
+                        <span>Total User</span>
+                      </div>
+                      <div className="float-end">
+                        <FontAwesomeIcon icon={faUser} className='bg-primary p-2 rounded-4 text-white' size="2x" />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
           <div className="col-md-12 mb-3">
             <div className="card border-primary">
               <div className="card-header bg-primary text-white">
@@ -151,7 +220,7 @@ const ManageUser = () => {
               </div>
             </div>
           </div>
-          <div className="col-md-6 mb-3">
+          {/* <div className="col-md-6 mb-3">
             <div className="card border-primary">
               <div className="card-header bg-primary text-white">
                 <FontAwesomeIcon icon={faKey} className="mr-2" /> Box 2
@@ -159,7 +228,7 @@ const ManageUser = () => {
               <div className="card-body">
               </div>
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
       <AddUserModal
